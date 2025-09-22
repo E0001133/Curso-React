4d8eefa52c9e270392679b4cbaabda3915146c38
Harold Cruz <harold.cruz@babelgroup.com>
2025-09-22 17:02:42 -0600

    feat: Se crea el archivo 01 y 02 para las dos clases realizadas del curso de variables

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,15 @@
+/*----- Variables -----*/
+
+/* let es una de claración de una variable que puede cambiar su valor */
+/* const es una declaración de una variable que se mantiene constante*/ 
+
+const firstName: string = 'Harold';
+const lastName = 'Cruz';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const constainsLetterU = lastName.includes('u');
+
+console.log({constainsLetterU, diceNumber})
+
