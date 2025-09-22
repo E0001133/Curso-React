6aa0751753df19e31f7d3dfae5fa6bcc0558dc32
Harold Cruz <harold.cruz@babelgroup.com>
2025-09-22 17:22:20 -0600

    feat: Se crea la clase para la clase 3, objetos literales.

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './bases/02-template-string'
+//import './bases/02-template-string'
+import './bases/03-object-lite'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
