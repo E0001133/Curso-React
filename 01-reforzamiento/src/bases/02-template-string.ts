7617b4e78beb330df3dc44db3f093780a23f114a
Harold Cruz <harold.cruz@babelgroup.com>
2025-09-22 17:10:39 -0600

    feat: Se termina de realizar el ejercicio de concatenación de caratceres.

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,8 @@
+const firstName = 'Harold';
+const lastName = "0'neal";
+
+/*Esto es una forma de concatenar variables string */
+const fullName = `El nombre es: ${firstName} ${lastName}`;
+console.log(fullName);
+
+console.log(firstName + ' ' + lastName + ':');
