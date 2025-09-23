dcf7a334945744da4738e02479d970fc67f63ffb
Harold Cruz <harold.cruz@babelgroup.com>
2025-09-23 11:46:31 -0600

    feat: Se crea el script para la clase de arreglos.

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 //import './bases/02-template-string'
 //import './bases/03-object-lite'
-import './bases/04-interfaces'
+//import './bases/04-interfaces'
+import './bases/05-arreglos'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
