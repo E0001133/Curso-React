3b5f0cc33987d20d73c825e11a00c7f6bfa6d951
Harold Cruz <harold.cruz@babelgroup.com>
2025-09-23 11:45:41 -0600

    feat: Se termina la clase de interfaces en typescript.

--- 01-reforzamiento/src/bases/04-interfaces.ts
+++ 01-reforzamiento/src/bases/04-interfaces.ts
@@ -1,9 +1,32 @@
-const engineer = {
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+
+    /**
+     * Esto es una propiedad opcional por el sibmolo ?
+   
+    address?: {
+        postalCode: string;
+        city: string;
+    }
+          */
+
+    address: Address;
+}
+
+interface Address {
+    postalCode: string;
+    city: string;
+}   
+
+const engineer: Person = {
     firstName: 'Harold',
     lastName: 'Cruz',
     age: 29,
+
     address:{
-        postalCode: 12345,
+        postalCode: '12345',
         city: 'CDMX',
     }
 };
